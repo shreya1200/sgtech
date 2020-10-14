827ed40261b400bc48c36107e35ff11c3fff40df
sahil2306 <56814542+sahil2306@users.noreply.github.com>
2020-10-15 00:26:40 +0530

    Home Page UI Changes

--- sgtech/src/app/app-page/app-page.component.ts
+++ sgtech/src/app/app-page/app-page.component.ts
@@ -18,26 +18,17 @@ export class AppPageComponent implements AfterViewInit {
     let canvas : HTMLCanvasElement = this.mycanvas.nativeElement;
     let ctx : CanvasRenderingContext2D = canvas.getContext("2d");
     
-    const img = new Image();
-    img.onload=this.start(canvas,ctx,img);
-    img.src = '../../assets/images/app.png';  
+    confetti.create(canvas)({
+      shapes: ['square'],
+      particleCount: 1000,
+      spread: 900,
+      origin: {
+          y: (1),
+          x: (0.5)
+      }
       
-  }
-
-  start(canvas:HTMLCanvasElement,ctx:CanvasRenderingContext2D,img):any {
-    // confetti.create(canvas)({
-    //   shapes: ['square'],
-    //   particleCount: 1000,
-    //   spread: 900,
-    //   origin: {
-    //       y: (1),
-    //       x: (0.5)
-    //   }
+    });
       
-    // });
-    ctx.drawImage(img,0,0,canvas.width,canvas.height);
-    ctx.stroke();
-    console.log(img);
   }
 }
 
